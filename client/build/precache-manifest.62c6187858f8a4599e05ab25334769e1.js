b16416460caf8011126a692719dfa6e014f3ab71
Marc Strong <marcstrong@yahoo.com>
2020-07-15 01:48:55 -0500

    initial commit

---
+++ client/build/precache-manifest.62c6187858f8a4599e05ab25334769e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2691dadb123e167587a7",
+    "url": "/static/css/main.59d8d281.chunk.css"
+  },
+  {
+    "revision": "2691dadb123e167587a7",
+    "url": "/static/js/main.91403e20.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c7df09a61a2c37d2f3a9",
+    "url": "/static/js/2.5c25870c.chunk.js"
+  },
+  {
+    "revision": "4e9226312c662b08b8a458dd5d0d59d8",
+    "url": "/index.html"
+  }
+];
